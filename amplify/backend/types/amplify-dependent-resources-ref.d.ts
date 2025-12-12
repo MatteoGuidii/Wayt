3103e52b0b5708a1a7f9275bf897fa462854dec0
MatteoGuidii <matteoguidi55@gmail.com>
2025-12-11 21:28:49 -0500

    refactor: Rename project and related configurations from 'zyvo' to 'Venuu' for consistency

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,12 +1,10 @@
 export type AmplifyDependentResourcesAttributes = {
   "auth": {
-    "Venuuf01d995f": {
+    "venuu5e6819c2": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
-      "HostedUIDomain": "string",
       "IdentityPoolId": "string",
       "IdentityPoolName": "string",
-      "OAuthMetadata": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
